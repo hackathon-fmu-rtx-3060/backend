4aaa546fe591336c7b6c776084508b73b1b28b2d
renan.beraldi <renanberaldi@outlook.com>
2024-12-06 12:23:30 -0300

    first commit

---
+++ src/main/java/com/compresuapeca/rtx4060/Rtx4060Application.java
@@ -0,0 +1,13 @@
+package com.compresuapeca.rtx4060;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rtx4060Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rtx4060Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/compresuapeca/rtx4060/Rtx4060ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compresuapeca.rtx4060;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rtx4060ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
